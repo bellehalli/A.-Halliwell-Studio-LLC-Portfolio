ac892a18a2c2566ac2619ba2933c5de8f0c39658
bellehalli <ahjanehadley9@gmail.com>
2026-09-17 23:29:03 -0400

    Reformat next.config.ts for consistency

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,7 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { poweredByHeader: false };
+
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+};
+
 export default nextConfig;
