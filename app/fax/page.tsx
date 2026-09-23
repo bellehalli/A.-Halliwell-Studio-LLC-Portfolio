dcc29c65d0522c2d97d2ad5a4b3005a417a213a8
bellehalli <ahjanehadley9@gmail.com>
2026-09-23 14:14:49 -0400

    Add signed-in private fax page

--- app/fax/page.tsx
+++ app/fax/page.tsx
@@ -1,10 +1,11 @@
-import { notFound } from "next/navigation";
-
-export const metadata = {
-  title: "A. Halliwell Fax",
-  robots: { index: false, follow: false },
-};
-
-export default function FaxPage() {
-  notFound();
+import { cookies } from "next/headers";
+import { hasFaxSession, SESSION_COOKIE } from "@/lib/fax-auth";
+import FaxClient from "./FaxClient";
+import FaxSignIn from "./FaxSignIn";
+import "./fax.css";
+export const dynamic = "force-dynamic";
+export const metadata = { title: "Private Fax Desk | A. Halliwell Studio", robots: { index: false, follow: false } };
+export default async function FaxPage() {
+  const session = (await cookies()).get(SESSION_COOKIE)?.value;
+  return hasFaxSession(session) ? <FaxClient /> : <FaxSignIn />;
 }
